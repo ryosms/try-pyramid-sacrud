9e02fea5e8b05b1aa4ecf803a5c4af02af450971
ryosms <ryo.sms@gmail.com>
2016-06-16 21:48:22 +0900

    Add routing tutorial

---
+++ pyramid-tutorial/routing.py
@@ -0,0 +1,25 @@
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.view import view_config
+from waitress import serve
+
+
+@view_config(route_name='home')
+def index(request):
+    return Response('home')
+
+
+@view_config(route_name='hello')
+def hello(request):
+    return Response('hello')
+
+
+if __name__ == '__main__':
+    config = Configurator()
+    config.add_route('home', '/')
+    config.add_route('hello', '/hello')
+
+    config.scan()
+
+    app = config.make_wsgi_app()
+    serve(app, host='0.0.0.0')
