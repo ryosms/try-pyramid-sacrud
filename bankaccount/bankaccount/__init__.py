d9e8217cd114044e4b8963712c718d2e757a21a7
ryosms <ryo.sms@gmail.com>
2016-06-18 02:30:53 +0900

    Add database settings
    and add libraries
    - sqlalchemy
    - pyramid_tm
    - sqlahelper

--- bankaccount/bankaccount/__init__.py
+++ bankaccount/bankaccount/__init__.py
@@ -1,11 +1,16 @@
+import sqlahelper
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    engine = engine_from_config(settings)
+    sqlahelper.add_engine(engine=engine)
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+    config.include('pyramid_tm')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
